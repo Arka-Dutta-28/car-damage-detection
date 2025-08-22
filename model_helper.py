a00931abc6866467047c217cf6d4ef3f3d966311
Arka Dutta <dutta280302@gmail.com>
2025-08-22 15:43:29 +0530

    updating the model_helper, inference in cpu

--- model_helper.py
+++ model_helper.py
@@ -43,8 +43,12 @@ def predict(image_path):
     global trained_model
 
     if trained_model is None:
+        device = torch.device("cpu")
         trained_model = CarClassifierResNet()
-        trained_model.load_state_dict(torch.load("model/saved_model.pth"))
+        trained_model.load_state_dict(
+            torch.load("model/saved_model.pth", map_location=device)
+        )
+        trained_model.to(device)
         trained_model.eval()
 
     with torch.no_grad():
